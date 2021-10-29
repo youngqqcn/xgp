0560e0cb734f52ba1665ef80bc1f01043538babc
youngqqcn <youngqqcn@gmail.com>
2021-10-29 22:36:41 +0800

    增加科大讯飞语音识别

---
+++ xpgwin/xpgwinDlg.cpp
@@ -0,0 +1,349 @@
+﻿
+// xpgwinDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "xpgwin.h"
+#include "xpgwinDlg.h"
+#include "afxdialogex.h"
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include <errno.h>
+
+
+
+#include "qtts.h"
+#include "msp_cmn.h"
+#include "msp_errors.h"
+
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CxpgwinDlg 对话框
+
+
+
+CxpgwinDlg::CxpgwinDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_XPGWIN_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CxpgwinDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CxpgwinDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDOK, &CxpgwinDlg::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// CxpgwinDlg 消息处理程序
+
+BOOL CxpgwinDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CxpgwinDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CxpgwinDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CxpgwinDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+
+
+/* wav音频头部格式 */
+typedef struct _wave_pcm_hdr
+{
+	char            riff[4];                // = "RIFF"
+	int				size_8;                 // = FileSize - 8
+	char            wave[4];                // = "WAVE"
+	char            fmt[4];                 // = "fmt "
+	int				fmt_size;				// = 下一个结构体的大小 : 16
+
+	short int       format_tag;             // = PCM : 1
+	short int       channels;               // = 通道数 : 1
+	int				samples_per_sec;        // = 采样率 : 8000 | 6000 | 11025 | 16000
+	int				avg_bytes_per_sec;      // = 每秒字节数 : samples_per_sec * bits_per_sample / 8
+	short int       block_align;            // = 每采样点字节数 : wBitsPerSample / 8
+	short int       bits_per_sample;        // = 量化比特数: 8 | 16
+
+	char            data[4];                // = "data";
+	int				data_size;              // = 纯数据长度 : FileSize - 44 
+} wave_pcm_hdr;
+
+/* 默认wav音频头部数据 */
+wave_pcm_hdr default_wav_hdr =
+{
+	{ 'R', 'I', 'F', 'F' },
+	0,
+	{'W', 'A', 'V', 'E'},
+	{'f', 'm', 't', ' '},
+	16,
+	1,
+	1,
+	16000,
+	32000,
+	2,
+	16,
+	{'d', 'a', 't', 'a'},
+	0
+};
+/* 文本合成 */
+int text_to_speech(const char* src_text, const char* des_path, const char* params)
+{
+	int          ret = -1;
+	FILE* fp = NULL;
+	const char* sessionID = NULL;
+	unsigned int audio_len = 0;
+	wave_pcm_hdr wav_hdr = default_wav_hdr;
+	int          synth_status = MSP_TTS_FLAG_STILL_HAVE_DATA;
+
+	if (NULL == src_text || NULL == des_path)
+	{
+		printf("params is error!\n");
+		return ret;
+	}
+	fp = fopen(des_path, "wb");
+	if (NULL == fp)
+	{
+		printf("open %s error.\n", des_path);
+		return ret;
+	}
+	/* 开始合成 */
+	sessionID = QTTSSessionBegin(params, &ret);
+	if (MSP_SUCCESS != ret)
+	{
+		printf("QTTSSessionBegin failed, error code: %d.\n", ret);
+		fclose(fp);
+		return ret;
+	}
+	ret = QTTSTextPut(sessionID, src_text, (unsigned int)strlen(src_text), NULL);
+	if (MSP_SUCCESS != ret)
+	{
+		printf("QTTSTextPut failed, error code: %d.\n", ret);
+		QTTSSessionEnd(sessionID, "TextPutError");
+		fclose(fp);
+		return ret;
+	}
+	printf("正在合成 ...\n");
+	fwrite(&wav_hdr, sizeof(wav_hdr), 1, fp); //添加wav音频头，使用采样率为16000
+	while (1)
+	{
+		/* 获取合成音频 */
+		const void* data = QTTSAudioGet(sessionID, &audio_len, &synth_status, &ret);
+		if (MSP_SUCCESS != ret)
+			break;
+		if (NULL != data)
+		{
+			fwrite(data, audio_len, 1, fp);
+			wav_hdr.data_size += audio_len; //计算data_size大小
+		}
+		if (MSP_TTS_FLAG_DATA_END == synth_status)
+			break;
+	}
+	printf("\n");
+	if (MSP_SUCCESS != ret)
+	{
+		printf("QTTSAudioGet failed, error code: %d.\n", ret);
+		QTTSSessionEnd(sessionID, "AudioGetError");
+		fclose(fp);
+		return ret;
+	}
+	/* 修正wav文件头数据的大小 */
+	wav_hdr.size_8 += wav_hdr.data_size + (sizeof(wav_hdr) - 8);
+
+	/* 将修正过的数据写回文件头部,音频文件为wav格式 */
+	fseek(fp, 4, 0);
+	fwrite(&wav_hdr.size_8, sizeof(wav_hdr.size_8), 1, fp); //写入size_8的值
+	fseek(fp, 40, 0); //将文件指针偏移到存储data_size值的位置
+	fwrite(&wav_hdr.data_size, sizeof(wav_hdr.data_size), 1, fp); //写入data_size的值
+	fclose(fp);
+	fp = NULL;
+	/* 合成完毕 */
+	ret = QTTSSessionEnd(sessionID, "Normal");
+	if (MSP_SUCCESS != ret)
+	{
+		printf("QTTSSessionEnd failed, error code: %d.\n", ret);
+	}
+
+	return ret;
+}
+
+int generate()
+{
+	int         ret = MSP_SUCCESS;
+	const char* login_params = "appid = 01e9f2eb, work_dir = .";//登录参数,appid与msc库绑定,请勿随意改动
+	/*
+	* rdn:           合成音频数字发音方式
+	* volume:        合成音频的音量
+	* pitch:         合成音频的音调
+	* speed:         合成音频对应的语速
+	* voice_name:    合成发音人
+	* sample_rate:   合成音频采样率
+	* text_encoding: 合成文本编码格式
+	*
+	*/
+	const char* session_begin_params = "engine_type = local, voice_name = xiaoyan, text_encoding = GB2312, tts_res_path = fo|res\\tts\\xiaoyan.jet;fo|res\\tts\\common.jet, sample_rate = 16000, speed = 50, volume = 50, pitch = 50, rdn = 2";
+	const char* filename = "tts_sample.wav"; //合成的语音文件名称
+	const char* text = "科大讯飞作为智能语音技术提供商，在智能语音技术领域有着长期的研究积累，并在中文语音合成、语音识别、口语评测等多项技术上拥有技术成果。科大讯飞是我国以语音技术为产业化方向的国家863计划产业化基地"; //合成文本
+	/* 用户登录 */
+	ret = MSPLogin(NULL, NULL, login_params); //第一个参数是用户名，第二个参数是密码，第三个参数是登录参数，用户名和密码可在http://www.xfyun.cn注册获取
+	if (MSP_SUCCESS != ret)
+	{
+		printf("MSPLogin failed, error code: %d.\n", ret);
+		goto exit;//登录失败，退出登录
+	}
+
+	printf("\n###########################################################################\n");
+	printf("## 语音合成（Text To Speech，TTS）技术能够自动将任意文字实时转换为连续的 ##\n");
+	printf("## 自然语音，是一种能够在任何时间、任何地点，向任何人提供语音信息服务的  ##\n");
+	printf("## 高效便捷手段，非常符合信息时代海量数据、动态更新和个性化查询的需求。  ##\n");
+	printf("###########################################################################\n\n");
+
+	/* 文本合成 */
+	printf("开始合成 ...\n");
+	ret = text_to_speech(text, filename, session_begin_params);
+	if (MSP_SUCCESS != ret)
+	{
+		printf("text_to_speech failed, error code: %d.\n", ret);
+	}
+	printf("合成完毕\n");
+
+exit:
+	printf("按任意键退出 ...\n");
+	//_getch();
+	MSPLogout(); //退出登录
+
+	return 0;
+}
+
+
+
+void CxpgwinDlg::OnBnClickedOk()
+{
+	generate();
+	// TODO: 在此添加控件通知处理程序代码
+	CDialogEx::OnOK();
+}
